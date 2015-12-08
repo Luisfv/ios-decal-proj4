6f3e76302c6d204866820d08ec817b90fb088756
Luis <alternateluis@live.com>
2015-12-07 18:34:33 -0800

    added parse support

---
+++ Habanow/ParseStarterProject/ViewController.swift
@@ -0,0 +1,40 @@
+/**
+* Copyright (c) 2015-present, Parse, LLC.
+* All rights reserved.
+*
+* This source code is licensed under the BSD-style license found in the
+* LICENSE file in the root directory of this source tree. An additional grant
+* of patent rights can be found in the PATENTS file in the same directory.
+*/
+
+import UIKit
+import Parse
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let user = PFUser()
+        user.username = "my name"
+        user.password = "my pass"
+        user.email = "email@example.com"
+        
+        // other fields can be set if you want to save more information
+        user["phone"] = "650-555-0000"
+        
+        user.signUpInBackgroundWithBlock { (success: Bool?, error: NSError?) -> Void in
+            if error == nil {
+                // Hooray! Let them use the app now.
+            } else {
+                // Examine the error object and inform the user.
+            }
+        }
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
